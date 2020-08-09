306df87c9124c3c68b7acddcd23552aaceb50e2c
Anna Ksiazek <aksiazek@softak.eu>
2020-08-09 14:52:03 +0200

    Task02-0: fix price type (from double to BigDecimal)

--- backend/src/test/java/pl/sdacademy/simple/util/DatabaseUtilIT.java
+++ backend/src/test/java/pl/sdacademy/simple/util/DatabaseUtilIT.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -23,11 +24,12 @@ class DatabaseUtilIT {
 	@Test
 	void shouldFindCars() {
 
-		Query query = entityManager.createQuery("select car from CarEntity car", CarEntity.class);
+		Query query = entityManager.createQuery("select car from CarEntity car order by car.id", CarEntity.class);
 
 		List<CarEntity> result = query.getResultList();
 
 		assertThat(result).hasSize(8);
 		assertThat(result.get(0).getAttributes()).hasSize(2);
+		assertThat(result.get(1).getPrice()).isEqualTo(BigDecimal.valueOf(7.5));
 	}
 }
